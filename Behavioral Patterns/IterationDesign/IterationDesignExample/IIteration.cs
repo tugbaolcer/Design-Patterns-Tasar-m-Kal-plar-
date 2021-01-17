ce2cc1d02a9fc659dda17460668f208320ea344d
tugbaolcer <35061098+tugbaolcer@users.noreply.github.com>
2021-01-17 17:51:18 +0300

    Add files via upload

---
+++ Behavioral Patterns/IterationDesign/IterationDesignExample/IIteration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationDesignExample
+{
+    public interface IIteration
+    {
+        //Bir sonraki elemenı getirir.
+        Personnel NextItem();
+        //Bir sonraki eylemde eleman var mı?
+        bool HasItem();
+        //Mevcut elemanı getirir.
+        Personnel CurrentItem();
+    }
+}
---
+++ Behavioral Patterns/IterationDesign/IterationDesignExample/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationDesignExample
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            PersonnelAggregate personnelAggregate = new PersonnelAggregate();
+            personnelAggregate.Add(new Personnel {PersonnelId=1, PersonnelName="Tuğba", PersonelLastName="ÖLÇER"});
+            personnelAggregate.Add(new Personnel { PersonnelId = 2, PersonnelName = "Fatih", PersonelLastName = "SARAL" });
+            personnelAggregate.Add(new Personnel { PersonnelId = 3, PersonnelName = "Pınar", PersonelLastName = "SAVCI" });
+            personnelAggregate.Add(new Personnel { PersonnelId = 4, PersonnelName = "Yasemin", PersonelLastName = "ÖZTÜRK" });
+
+            IIteration iteration = personnelAggregate.CreateIterator();
+
+            while (iteration.HasItem())
+            {
+                Console.WriteLine($"ID : {iteration.CurrentItem().PersonnelId} \n" +
+                    $"Personnel Name : {iteration.CurrentItem().PersonnelName} \n" +
+                    $"Personnel LastName : {iteration.CurrentItem().PersonelLastName} \n***************************");
+                iteration.NextItem();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
